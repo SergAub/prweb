f4e243d81c9b43b206804bf05adb6c6c41a0e561
SergAub <92084092+SergAub@users.noreply.github.com>
2024-12-08 21:25:41 +0700

    Пофикшены тени в списке тарифов

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <footer>
-            <div className="bg-backgroundDark p-8 shadow-md">
-                <p>© 2024 ПР-21.102 Аюбов.</p>
+            <div className="bg-backgroundDark p-8 shadow-up">
+                <p>© 2024 ПР-21.102 Аюбов</p>
             </div>
         </footer>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
+"use client";
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Image from "next/image";
+import {
+    useEffect,
+    useState
+} from "react";
 
 export default function Home() {
-  return (
+    const [textContent, setTextContent] = useState('');
+
+    useEffect(() => {
+        const fetchText = async () => {
+            const response = await fetch('/main.txt');
+            const text = await response.text();
+            setTextContent(text);
+        };
+
+        fetchText();
+    }, []);
+
+    return (
     <div>
       <Header/>
         <div className="m-10">
-            <p>Cubes&Mods - Майнкрафт хостинг</p>
-            <ol className="flex space-x-8 my-4 overflow-x-auto justify-center">
+            <p className="mb-6">{textContent}</p>
+            <ol className="flex space-x-8 my-4 p-4 overflow-x-auto justify-center">
                 <li className="bg-backgroundDark shadow-md py-8 px-10 flex flex-col items-center">
                     <Image
                         src="/Placeholder.png"
@@ -20,7 +38,7 @@ export default function Home() {
                     <div className="text-md">
                         <p>Ядра: 1шт.</p>
                         <p>ОЗУ: 1гб.</p>
-                        <p>Время аренды: 10ч.</p>
+                        <p>Время аренды: 24ч.</p>
                         <p>Цена: 300руб.</p>
                     </div>
                     <button className="button mt-6">Купить</button>
@@ -32,12 +50,12 @@ export default function Home() {
                         width={196}
                         height={196}
                     />
-                    <h2 className="text-2xl m-3">Бесплатный</h2>
+                    <h2 className="text-2xl m-3">Начальный</h2>
                     <div className="text-md">
-                        <p>Ядра: 1шт.</p>
-                        <p>ОЗУ: 1гб.</p>
-                        <p>Время аренды: 10ч.</p>
-                        <p>Цена: 300руб.</p>
+                        <p>Ядра: 2шт.</p>
+                        <p>ОЗУ: 2гб.</p>
+                        <p>Время аренды: 360ч.</p>
+                        <p>Цена: 500руб.</p>
                     </div>
                     <button className="button mt-6">Купить</button>
                 </li>
@@ -48,12 +66,12 @@ export default function Home() {
                         width={196}
                         height={196}
                     />
-                    <h2 className="text-2xl m-3">Бесплатный</h2>
+                    <h2 className="text-2xl m-3">Средний</h2>
                     <div className="text-md">
-                        <p>Ядра: 1шт.</p>
-                        <p>ОЗУ: 1гб.</p>
-                        <p>Время аренды: 10ч.</p>
-                        <p>Цена: 300руб.</p>
+                        <p>Ядра: 6шт.</p>
+                        <p>ОЗУ: 8гб.</p>
+                        <p>Время аренды: 720ч.</p>
+                        <p>Цена: 1500руб.</p>
                     </div>
                     <button className="button mt-6">Купить</button>
                 </li>
@@ -64,12 +82,12 @@ export default function Home() {
                         width={196}
                         height={196}
                     />
-                    <h2 className="text-2xl m-3">Бесплатный</h2>
+                    <h2 className="text-2xl m-3">Максимальный</h2>
                     <div className="text-md">
-                        <p>Ядра: 1шт.</p>
-                        <p>ОЗУ: 1гб.</p>
-                        <p>Время аренды: 10ч.</p>
-                        <p>Цена: 300руб.</p>
+                        <p>Ядра: 8шт.</p>
+                        <p>ОЗУ: 16гб.</p>
+                        <p>Время аренды: 2160ч..</p>
+                        <p>Цена: 3300руб.</p>
                     </div>
                     <button className="button mt-6">Купить</button>
                 </li>
